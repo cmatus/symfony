70aebc9d538e61d66fa1262a2f767c7ea62e58be
Claudio Matus Alegría <claudio@matus.cl>
2017-01-26 18:27:33 -0300

    Commit masivo

--- 001/src/AppBundle/Controller/DefaultController.php
+++ 001/src/AppBundle/Controller/DefaultController.php
@@ -8,25 +8,65 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="raiz")
      */
     public function indexAction(Request $request)
     {
-        /*
-        return $this->render('login/login.html.twig', [
-            'cliente' => array(
+        return $this->render('index.html.twig', [
+            'datos' => array(
                 'nombre' => $this->getParameter('app.cliente'),
-                'aplicacion' => $this->getParameter('app.nombre')
+                'aplicacion' => $this->getParameter('app.nombre'),
+                'tabCliente' => $this->getParameter('app.tabCliente'),
+                'datos' => $clientes
             )
         ]);
-        */
+    }
+
+    /**
+     * @Route("/cliente/listar", name="clienteListar")
+     */
+    public function listarAction(Request $request)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $clientes = $em->getRepository('AppBundle:Cliente')->findAll();
+        
         return $this->render('cliente/listar.html.twig', [
             'cliente' => array(
                 'nombre' => $this->getParameter('app.cliente'),
                 'aplicacion' => $this->getParameter('app.nombre'),
                 'tabCliente' => $this->getParameter('app.tabCliente'),
+                'datos' => $clientes
+            )
+        ]);
+
+    }
+
+    /**
+     * @Route("/cliente/editar/{rut}", name="clienteEditar")
+     */
+    public function editarAction($rut)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $cliente = $em->getRepository('AppBundle:Cliente')->findOneBy(array('rut' => $rut));
+        return $this->render('cliente/editar.html.twig', [
+            'cliente' => array(
+                'nombre' => $this->getParameter('app.cliente'),
+                'aplicacion' => $this->getParameter('app.nombre'),
+                'tabCliente' => $this->getParameter('app.tabCliente'),
+                'datos' => $cliente
             )
         ]);
     }
+    /*
+    private function createCreateForm(cliente $entity)
+    {
+        $form = $this->createForm(new clienteType(), $entity, array(
+            'action' => $this->generateUrl('mrf_clientes_create'), 
+            'method' => 'POST'));   
+        return $form;
+    }
+    */
 }
---
+++ 001/src/AppBundle/Entity/Cliente.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cliente
+ *
+ * @ORM\Table(name="cliente")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ClienteRepository")
+ */
+class Cliente
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rut", type="string", length=10, unique=true)
+     */
+    private $rut;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="direccion", type="string", length=255)
+     */
+    private $direccion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comuna", type="string", length=255)
+     */
+    private $comuna;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set rut
+     *
+     * @param string $rut
+     *
+     * @return Cliente
+     */
+    public function setRut($rut)
+    {
+        $this->rut = $rut;
+
+        return $this;
+    }
+
+    /**
+     * Get rut
+     *
+     * @return string
+     */
+    public function getRut()
+    {
+        return $this->rut;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Cliente
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set direccion
+     *
+     * @param string $direccion
+     *
+     * @return Cliente
+     */
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+
+    /**
+     * Get direccion
+     *
+     * @return string
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * Set comuna
+     *
+     * @param string $comuna
+     *
+     * @return Cliente
+     */
+    public function setComuna($comuna)
+    {
+        $this->comuna = $comuna;
+
+        return $this;
+    }
+
+    /**
+     * Get comuna
+     *
+     * @return string
+     */
+    public function getComuna()
+    {
+        return $this->comuna;
+    }
+}
+
