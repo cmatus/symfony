45736533ab087ea2a71237520500195337a3f9dc
Claudio Matus Alegría <Claudio Matus Alegra>
2017-01-27 01:34:09 -0300

    Commit masivo

--- 001/src/AppBundle/Controller/DefaultController.php
+++ 001/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,10 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Cliente;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -60,6 +64,31 @@ class DefaultController extends Controller
             )
         ]);
     }
+
+    /**
+     * @Route("/cliente/agregar/", name="clienteAgregar")
+     */
+    public function newAction(Request $request)
+    {
+        $cliente = new Cliente();
+        $form = $this->createFormBuilder($cliente)
+            ->add('rut', TextType::class)
+            ->add('nombre', TextType::class)
+            ->add('direccion', TextType::class)
+            ->add('comuna', TextType::class)
+            ->add('Registrar', SubmitType::class)
+            ->getForm();
+        return $this->render('cliente/editar.html.twig', array(
+            'form' => $form->createView(),
+             'cliente' => array(
+                'nombre' => $this->getParameter('app.cliente'),
+                'aplicacion' => $this->getParameter('app.nombre'),
+                'tabCliente' => $this->getParameter('app.tabCliente'),
+                'datos' => $cliente
+            )
+        ));
+    }
+
     /*
     private function createCreateForm(cliente $entity)
     {
