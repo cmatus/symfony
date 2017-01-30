98a8befe65ac65b1ace6670774a4cd3cf473cf61
Claudio Matus Alegría <claudio@matus.cl>
2017-01-30 19:58:45 -0300

    Commit masivo

--- 001/src/Gibson/ClienteBundle/Controller/DefaultController.php
+++ 001/src/Gibson/ClienteBundle/Controller/DefaultController.php
@@ -6,8 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+
     public function indexAction()
     {
         return $this->render('GibsonClienteBundle:Default:index.html.twig');
     }
+
+    public function listarAction()
+    {
+        return $this->render('GibsonClienteBundle:Default:listar.html.twig');
+    }
+
+    public function agregarAction()
+    {
+        return $this->render('GibsonClienteBundle:Default:agregar.html.twig');
+    }
+
 }
