9a77e162c5af50ebb0827cf3a6a15f967d192701
Claudio Matus Alegría <Claudio Matus Alegra>
2017-01-30 01:12:11 -0300

    Commit masivo

---
+++ 001/src/Gibson/ClienteBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Gibson\ClienteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('GibsonClienteBundle:Default:index.html.twig');
+    }
+}
